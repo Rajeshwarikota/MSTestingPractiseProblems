34dcf6627a447bcc729aee604357cd8d0c19c768
“Rajeshwari <kotarajeswari003@gmail.com>
2023-07-19 18:06:52 +0530

    [Rajeshwari kota] Adding MS Testing Problems

---
+++ MSTestingProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTestingProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("....MS Testing Problems....");
+            Console.ReadLine();
+        }
+    }
+}
